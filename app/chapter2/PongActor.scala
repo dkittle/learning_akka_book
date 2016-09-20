f7cb78d065eb2510531f77bbbb052c033a452f42
dkittle <don@kittle.ca>
2016-09-20 18:34:51 -0400

    chapter 2 following along

---
+++ app/chapter2/PongActor.scala
@@ -0,0 +1,21 @@
+package chapter2
+
+import PongActor._
+import akka.actor.{Actor, Props, Status}
+
+class PongActor(response: String) extends Actor {
+
+  override def receive = {
+    case "Ping" => sender() ! response
+    case "Pong" => sender() ! "Deja Vu"
+    case _ => sender() ! Status.Failure(new Exception("unknown message"))
+  }
+
+}
+
+object PongActor {
+
+  def props(response: String): Props = {
+    Props(classOf[PongActor], response)
+  }
+}
